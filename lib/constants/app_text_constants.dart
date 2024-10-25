d7fc8f8a62c5fe6357fd273037483b7a04783eee
Bhoj Raj Mishra <bhojrajmishra4@gmail.com>
2024-10-25 15:30:33 +0545

    refactor: refactor fastlane code

---
+++ lib/constants/app_text_constants.dart
@@ -0,0 +1,35 @@
+class AppTextConstants {
+  //LoginView Constants
+  static const pleaseLogin = 'Please login to your account using';
+  static const signUp = "Sign Up";
+  static const forgetPasswordd = 'Forgot Password?';
+  static const procced = "Continue";
+  static const signWithGoogle = 'Please login to your account using';
+  static const signWithFacebok = 'Sign-in with Facebook';
+  static const labelTextForDivider = 'Or continue with email';
+  static const labelTextForEmail = "Email";
+  static const labelTextForPassword = "Password";
+
+  //homeView Constants
+  static const newReleaseTitle = "New Releases";
+  static const recentEbook = "Recent E-books";
+  static const recentAudioBook = "Recent Audiobooks";
+  static const bestSeller = "Best Sellers";
+  static const viewAll = 'View All';
+
+  //DetailView Strings
+  static const bookDetailTitle = "Book Details";
+  static const paperbackTitle = 'Paperback';
+  static const ebookTitle = 'Ebook';
+  static const descriptionTitle = 'Description';
+  static const buyNowText = 'Buy Now';
+  static const addTOcartText = "Add to Cart";
+
+  //cartView Strings
+  static const cartTitle = "Cart";
+  static const checkOutTitle = "Checkout";
+
+  //registration Strings
+  static const signUpText = "Sign Up";
+  static const alreadyHaveAnAccountText = "Already have an account?";
+}
